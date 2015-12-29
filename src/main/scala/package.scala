33c33fe70fa90ae2fca21d4e66e96b73a7eb03b2
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-12-29 19:10:30 +0100

    add illumina code from ohnosequences/datasets#26

---
+++ src/main/scala/package.scala
@@ -0,0 +1,10 @@
+package ohnosequences.datasets
+
+package object illumina {
+
+  type SingleEndType = singleEndType.type
+  val SingleEndType: SingleEndType = singleEndType
+
+  type PairedEndType = pairedEndType.type
+  val PairedEndType: PairedEndType = pairedEndType
+}
